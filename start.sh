439bb0ee671e07acbf974e77e2532de45cbf303e
iizukar <czechia.deitan@gmail.com>
2025-03-05 00:43:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# Start services sequentially to save resources
 
-# Virtual display with smaller resolution
-Xvfb :1 -screen 0 1024x768x16 &
+# Cleanup previous X server locks
+rm -f /tmp/.X1-lock
+
+# Start virtual framebuffer
+Xvfb :1 -screen 0 1024x768x16 +extension GLX +render -noreset &
 export DISPLAY=:1
 
-# Start window manager
+# Start fluxbox with basic config
 fluxbox &
 
-# Start VNC server with low quality settings
-x11vnc -display :1 -rfbauth /root/.vnc/passwd -forever -shared -noxdamage -localhost -rfbport 5901 -bg
+# Start VNC server
+x11vnc -display :1 -rfbauth /root/.vnc/passwd -forever -shared -noxdamage -localhost -rfbport 5901 &
 
-# Start noVNC with basic auth
-/opt/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen ${PORT:-8080} --heartbeat 30
+# Start noVNC with bundled websockify
+/opt/noVNC-1.4.0/utils/novnc_proxy \
+    --vnc localhost:5901 \
+    --listen ${PORT:-8080} \
+    --heartbeat 30 \
+    --cert /opt/noVNC-1.4.0/utils/self.pem
