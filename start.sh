d7da1f6f7d9c8826b6d820467bb81cb4476f0ff0
iizukar <czechia.deitan@gmail.com>
2025-03-05 00:54:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Cleanup previous locks
 rm -f /tmp/.X1-lock
 
-# Start Xvfb with basic configuration
+# Start Xvfb
 Xvfb :1 -screen 0 1024x768x16 +extension GLX +render -noreset &
 export DISPLAY=:1
 
@@ -13,9 +13,9 @@ fluxbox &
 # Start VNC server
 x11vnc -display :1 -rfbauth /root/.vnc/passwd -forever -shared -noxdamage -localhost -rfbport 5901 &
 
-# Start noVNC with generated certificate
-/opt/noVNC/utils/novnc_proxy \
+# Start noVNC with pre-installed websockify
+/opt/noVNC-1.4.0/utils/novnc_proxy \
     --vnc localhost:5901 \
     --listen ${PORT:-8080} \
     --heartbeat 30 \
-    --cert /opt/noVNC/utils/self.pem
+    --cert /opt/noVNC-1.4.0/utils/self.pem
