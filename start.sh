f7d25040d0eab0e3cdedb8efd14ef830ba135377
iizukar <czechia.deitan@gmail.com>
2025-03-05 01:02:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Cleanup previous instances
+# Cleanup previous locks
 rm -f /tmp/.X1-lock
 rm -f /tmp/.X11-unix/X1
 
-# Start X virtual framebuffer
+# Start virtual display
 Xvfb :1 -screen 0 1024x768x16 +extension GLX +render -noreset &
 export DISPLAY=:1
 
@@ -14,7 +14,7 @@ fluxbox &
 # Start VNC server
 x11vnc -display :1 -rfbauth /root/.vnc/passwd -forever -shared -noxdamage -localhost -rfbport 5901 &
 
-# Start noVNC with full path to websockify
+# Start noVNC
 /opt/noVNC-1.4.0/utils/novnc_proxy \
     --vnc localhost:5901 \
     --listen ${PORT:-8080} \
